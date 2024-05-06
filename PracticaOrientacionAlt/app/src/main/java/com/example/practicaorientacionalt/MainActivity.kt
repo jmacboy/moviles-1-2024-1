df810358fd80dd520fbbaaf11cbb948ce12cefec
jmacboy <j.m.alvarez.camacho@gmail.com>
2024-05-06 13:52:02 -0400

    agregados proyectos de orientación

---
+++ PracticaOrientacionAlt/app/src/main/java/com/example/practicaorientacionalt/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.practicaorientacionalt
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicaorientacionalt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setupEventListeners()
+    }
+
+    private fun setupEventListeners() {
+        binding.btnSave.setOnClickListener {
+            Toast.makeText(this, "Guardar presionado", Toast.LENGTH_SHORT).show()
+        }
+        binding.btnCancel?.setOnClickListener {
+            Toast.makeText(this, "Cancelar presionado", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ PracticaOrientacionConfig/app/src/main/java/com/example/practicaorientacionconfig/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.practicaorientacionconfig
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.practicaorientacionconfig.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //horizontal
+            binding.btnHideLandscape.visibility = View.GONE
+            Toast.makeText(this, "Horizontal", Toast.LENGTH_SHORT).show()
+        } else {
+            //vertical
+            binding.btnHideLandscape.visibility = View.VISIBLE
+            Toast.makeText(this, "Vertical", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
